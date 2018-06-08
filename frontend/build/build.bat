52cd5f48607e7c474311f87e777ffd5033ce9202
Hanson Jang (장재덕) <jdjang@bistel.com>
2018-06-08 10:19:26 +0900

    change build script

--- frontend/build/build.bat
+++ frontend/build/build.bat
@@ -7,6 +7,6 @@ cd ..\pdm-web
 call npm cache clean 
 call npm install
 call gulp build.prod --base=/portal/ --env=prod --config-env=prod
-call xcopy .\dist\prod  ..\..\backend\pdm-portal\src\main\webapp\resources\  /s /e /h /y
+call xcopy .\dist\prod  ..\..\backend\pdm-portal\portal\src\main\webapp\resources\  /s /e /h /y
 
 echo "eHMP Frontend Build finished."
