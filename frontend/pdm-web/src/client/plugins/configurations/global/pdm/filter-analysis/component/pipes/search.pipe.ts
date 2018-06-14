e7b9f21285782a70a10bbb753d1370f1afa967f3
mitchell <minwankim@bistel.com>
2018-06-14 11:28:33 -0700

    multicombo number filter 추가 in filter analysis

--- frontend/pdm-web/src/client/plugins/configurations/global/pdm/filter-analysis/component/pipes/search.pipe.ts
+++ frontend/pdm-web/src/client/plugins/configurations/global/pdm/filter-analysis/component/pipes/search.pipe.ts
@@ -10,7 +10,22 @@ export class SearchComboPipe implements PipeTransform {
         let filterdValues = [];
         if ( args['term'] !== '') {    
             let term: string = args['term'].toLocaleLowerCase();
-            filterdValues = value.filter(item => this.isContain(term,item, args['naming']))
+            let fromTo = [];
+            if(term.indexOf('[')>=0){
+                if( term.indexOf(']')>=0 && term.indexOf('-')>=0){
+                    let retval = term.match(/\[.*?\]/g)[0];
+                    if(retval.length>0){
+                        term = term.replace(retval,'');
+                        retval = retval.replace('[','');
+                        retval = retval.replace(']','');
+                        fromTo = retval.split('-');
+                    }
+                }else{
+                    term = term.substr(0,term.indexOf('['));
+                }
+    
+            }
+            filterdValues = value.filter(item => this.isContain(term,item,fromTo, args['naming']))
             return filterdValues;
         } else {
             filterdValues = value;
@@ -18,9 +33,25 @@ export class SearchComboPipe implements PipeTransform {
         }
     }
 
-    isContain(term: string, item: any, naming: string): any{
+    isContain(term: string, item: any,fromTo:any[], naming: string): any{
         if ( item[naming].toLocaleLowerCase().indexOf(term) !== -1 ) {
-            return item;
+            if(fromTo.length>0){
+                const  val = this.getNumber(item[naming]);
+                if(val!=null && val>=fromTo[0] && val<=fromTo[1]){
+                    return item;
+                }
+            }else{
+                return item;
+            }
+            
+        }
+    }
+    getNumber(str){
+        let data = str.match(/[+-]?\d+(?:\.\d+)?/g).map(Number);
+        if(data.length>1){
+            return data[0]
+        }else{
+            return null;
         }
     }
 }
