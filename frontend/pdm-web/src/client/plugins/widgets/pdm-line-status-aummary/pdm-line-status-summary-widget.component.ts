2234c12bddd3b03588f3757936e2eccd3a5d473e
Insu Kim <insukim@Insuui-MacBook-Pro.local>
2018-06-26 13:54:00 +0900

    feat: Modify alarm count and line status summary widget

--- frontend/pdm-web/src/client/plugins/widgets/pdm-line-status-aummary/pdm-line-status-summary-widget.component.ts
+++ frontend/pdm-web/src/client/plugins/widgets/pdm-line-status-aummary/pdm-line-status-summary-widget.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
 import { WidgetApi, WidgetRefreshType, OnSetup } from '../../../common';
 
+import * as IDataType from './model/data-type.interface';
+
 @Component({
     moduleId: module.id,
     selector: 'pdm-line-status-summary-widget',
@@ -8,6 +10,13 @@ import { WidgetApi, WidgetRefreshType, OnSetup } from '../../../common';
     styleUrls: ['./pdm-line-status-summary-widget.css']
 })
 export class PdmLineStatusSummaryWidgetComponent extends WidgetApi implements OnInit {
+
+    viewTimePriod: any = {
+        fromDate: 0,
+        toDate: 0
+    };
+
+    condition: IDataType.ContitionType;
     private _props: any;
 
     constructor() {
@@ -26,18 +35,36 @@ export class PdmLineStatusSummaryWidgetComponent extends WidgetApi implements On
         if (type === A3_WIDGET.APPLY_CONFIG_REFRESH || type === A3_WIDGET.JUST_REFRESH) {
             this.showSpinner();
             this._props = data;
-            this._setConfigInfo();
+            this._setConfigInfo(this._props);
         } else if (type === A3_WIDGET.SYNC_INCONDITION_REFRESH) {
-            
+
         }
     }
 
-    _setConfigInfo() {
-        
+    _setConfigInfo(props: any) {
+        let now: Date = new Date();
+        const startOfDay: Date = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+        const to: Date = startOfDay; // today 00:00:00
+
+        this.condition = {
+            fabId: props[CD.PLANT_ID],
+            timePeriod: {
+                from: props[CD.TIME_PERIOD]['from'],
+                to: to
+            }
+        };
+
+        this.viewTimePriod.fromDate = this.covertDateFormatter(props[CD.TIME_PERIOD]['from']);
+        this.viewTimePriod.toDate = this.covertDateFormatter(to);
+    }
+
+    covertDateFormatter(timestamp: Date): string {
+        const date = new Date(timestamp);
+        return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} 00:00`;
     }
 
     endChartLoad(ev: any) {
-        if(ev) {
+        if (ev) {
             this.hideSpinner();
         }
     }
@@ -46,6 +73,6 @@ export class PdmLineStatusSummaryWidgetComponent extends WidgetApi implements On
         this.showSpinner();
         // this.setGlobalLabel();
         this._props = this.getProperties();
-        this._setConfigInfo();
+        this._setConfigInfo(this._props);
     }
 }
