ae7e35848dd2b07c9fd9e2c32df562ca23e21f00
Chris Jung <jwjung@bistel.com>
2018-08-21 14:53:12 +0900

    Analysis Tool AVG -> MEAN으로 변경

--- backend/pdm-portal/portal/src/main/java/com/bistel/a3/portal/domain/pdm/AnalysisParamGroup.java
+++ backend/pdm-portal/portal/src/main/java/com/bistel/a3/portal/domain/pdm/AnalysisParamGroup.java
@@ -7,12 +7,15 @@ public class AnalysisParamGroup {
 	public String getParam_name() {
 		return param_name;
 	}
+	
 	public void setParam_name(String param_name) {
 		this.param_name = param_name;
 	}
+	
 	public String getGroup_name() {
 		return group_name;
 	}
+	
 	public void setGroup_name(String group_name) {
 		this.group_name = group_name;
 	}
