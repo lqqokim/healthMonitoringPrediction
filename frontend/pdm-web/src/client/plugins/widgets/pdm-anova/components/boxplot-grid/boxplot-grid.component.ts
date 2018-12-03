db85bb6462e7cd67230818ca609ad63c73696500
LEO <sykim1@bistel.com>
2018-12-03 11:51:00 +0900

    anova

--- frontend/pdm-web/src/client/plugins/widgets/pdm-anova/components/boxplot-grid/boxplot-grid.component.ts
+++ frontend/pdm-web/src/client/plugins/widgets/pdm-anova/components/boxplot-grid/boxplot-grid.component.ts
@@ -37,6 +37,7 @@ export class BoxplotGridComponent implements OnChanges, OnDestroy {
         setTimeout(() => {
             Plotly.newPlot(this.chartId, data, layout, config);
         }, 1000);
+        this.resize(data);
     }
 
     getBoxplotData(): any {
@@ -54,19 +55,33 @@ export class BoxplotGridComponent implements OnChanges, OnDestroy {
     private _setBoxplotData(): any {
         let data = [];
 
-        for (let i = 0; i < 100; i++) {
-            let y = [];
+        // for (let i = 0; i < 100; i++) {
+        //     let y = [];
 
-            for (let j = 0; j < 50; j++) {
-                const ran = Math.random() * (6000 - 100) + 100;
-                y.push(ran);
-            }
+        //     for (let j = 0; j < 50; j++) {
+        //         const ran = Math.random() * (6000 - 100) + 100;
+        //         y.push(ran);
+        //     }
 
+        //     data.push({
+        //         y: y,
+        //         type: 'box',
+        //         // visible: 'rangeslideronly'
+        //     });
+        // }
+        for(let i=0; i<100; i++){ 
             data.push({
-                y: y,
-                type: 'box',
-                // visible: 'rangeslideronly'
-            });
+              y: ['1', '2', '3', '4', '5'], 
+              boxpoints: false, 
+              type: 'box', 
+              name: 
+              `<sup><b>TOHS${i}</br></br>
+              Count:5855909 </br>
+              Median:5855909 </br>
+              Average:5855909 </br>
+              Standard Deviation:5855909 </br></b></sup>
+              `, 
+            })
         }
 
         return data;
@@ -86,7 +101,7 @@ export class BoxplotGridComponent implements OnChanges, OnDestroy {
             },
             annotations: [],
             xaxis: {
-                range: [0, 10], // this is the range the *selected area* of the slider spans
+                range: [-0.4, 5], // this is the range the *selected area* of the slider spans
                 rangeslider: {
                     borderwidth: 2,
                     thickness: 0.1,
@@ -125,6 +140,61 @@ export class BoxplotGridComponent implements OnChanges, OnDestroy {
             var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
             return "C" + v.toString(16);
         });
+    }
+
+    resize(data:any){
+        $(window).resize(function() {
+            const plotDiv = $('#myDiv > div > div > svg:nth-child(1) > g.draglayer.cursor-crosshair > g > rect.nsewdrag.drag.cursor-ew-resize');
+            // console.warn(plotDiv.width());
+            var boxplotChartWidth =  $( window ).width();
+            let dataCount = data.length;
+            let update;
+            // console.warn(boxplotChartWidth);
+            if(boxplotChartWidth < 440){
+              update = { 'xaxis.range': [-0.4, 1] };                    
+              Plotly.relayout('myDiv', update);                  
+            }
+            if(boxplotChartWidth > 440 && boxplotChartWidth > 600){
+              update = { 'xaxis.range': [-0.4, 2] };                    
+              Plotly.relayout('myDiv', update);                  
+            }
+            if(boxplotChartWidth > 600 && boxplotChartWidth > 755){
+              update = { 'xaxis.range': [-0.4, 3] };                    
+              Plotly.relayout('myDiv', update);                  
+            }
+            if(boxplotChartWidth > 755 && boxplotChartWidth > 910){
+              update = { 'xaxis.range': [-0.4, 4] };                    
+              Plotly.relayout('myDiv', update);                  
+            }  
+            if(boxplotChartWidth > 910 && boxplotChartWidth > 1060){
+              update = { 'xaxis.range': [-0.4, 5] };                    
+              Plotly.relayout('myDiv', update);                 
+            }  
+            if(boxplotChartWidth > 1060 && boxplotChartWidth > 1212){
+              update = { 'xaxis.range': [-0.4, 6] };                    
+              Plotly.relayout('myDiv', update);                  
+            }
+            if(boxplotChartWidth > 1212 && boxplotChartWidth > 1400){
+              update = { 'xaxis.range': [-0.4, 7] };                    
+              Plotly.relayout('myDiv', update);                  
+            }
+            if(boxplotChartWidth > 1400 && boxplotChartWidth > 1560){
+              update = { 'xaxis.range': [-0.4, 8] };                    
+              Plotly.relayout('myDiv', update);                  
+            }
+            if(boxplotChartWidth > 1560 && boxplotChartWidth > 1680){
+              update = { 'xaxis.range': [-0.4, 9] };                    
+              Plotly.relayout('myDiv', update);                  
+            }
+            if(boxplotChartWidth > 1680 && boxplotChartWidth > 1840){
+              update = { 'xaxis.range': [-0.4, 10] };                    
+              Plotly.relayout('myDiv', update);                  
+            }  
+            if(boxplotChartWidth > 1840){
+              update = { 'xaxis.range': [-0.4, 11] };                    
+              Plotly.relayout('myDiv', update);                  
+            }
+          });
     }
 
     ngOnDestroy() {
