9f15f2c62514ffd0fd32f92ce94b0e1f09835cda
insukim <kis420404@gmail.com>
2018-12-03 17:12:35 +0900

    MPPDM-768

--- frontend/pdm-web/src/client/plugins/widgets/pdm-correlation/components/scatter/scatter.component.ts
+++ frontend/pdm-web/src/client/plugins/widgets/pdm-correlation/components/scatter/scatter.component.ts
@@ -1,11 +1,26 @@
 import { Component, Input, Output, OnInit, OnChanges, ViewEncapsulation, EventEmitter, SimpleChanges } from '@angular/core';
-import { scatterData } from './../../model/mock-data';
+import * as ICorrelation from './../../model/correlation-interface';
 
 declare let Plotly: any;
 
 export interface Scatter {
-
+    x: Array<number>;
+    y: Array<number>;
+    mode: string;
+    name: string;
+    text?: Array<string>;
+    marker: {
+        color: string;
+        size: number;
+        line: {
+            color: string;
+            width: number;
+        }
+    },
+    type: string;
 }
+
+export type ScatterData = Array<Scatter>;
 
 @Component({
     moduleId: module.id,
@@ -15,7 +30,7 @@ export interface Scatter {
     encapsulation: ViewEncapsulation.None
 })
 export class ScatterComponent implements OnInit, OnChanges {
-    @Input() data;
+    @Input() data: ICorrelation.Response;
 
     chartId: string = this._guid();;
 
@@ -29,13 +44,13 @@ export class ScatterComponent implements OnInit, OnChanges {
 
     ngOnChanges(changes: SimpleChanges) {
         if (changes && changes['data']['currentValue']) {
-            console.log('ngOnChanges scatter => ', changes);
-            const data = changes['data']['currentValue'];
+            const data: ICorrelation.Response = changes['data']['currentValue'];
+
             this.drawScatter(data);
         }
     }
 
-    drawScatter(data): void {
+    drawScatter(data: ICorrelation.Response): void {
         const scatterData: any = this.getScatterData(data);
         const layout: any = this.getScatterLayout(data);
         const config: any = this.getScatterConfig();
@@ -43,7 +58,7 @@ export class ScatterComponent implements OnInit, OnChanges {
         Plotly.newPlot(this.chartId, scatterData, layout, config);
     }
 
-    getScatterData(data): any {
+    getScatterData(data) {
         return this._setScatterData(data);
     }
 
@@ -55,10 +70,43 @@ export class ScatterComponent implements OnInit, OnChanges {
         return this._setScatterLayout(data);
     }
 
-    private _setScatterData(data): any {
-        // parse data
+    private _setScatterData(data: ICorrelation.Response) {
+        let xValues: Array<number> = [];
+        let yValues: Array<number> = [];
 
-        return scatterData;
+        data.correlationScatter.map((point: Array<number>) => {
+            xValues.push(point[0]);
+            yValues.push(point[1]);
+        });
+
+        const line = {
+            mode: 'lines',
+            name: 'Regression',
+            x: [data.regression.start_xValue, data.regression.end_xValue],
+            y: [data.regression.start_yValue, data.regression.end_yValue],
+            line: {
+                color: 'red'
+            }
+        }
+
+        const scatterData: Scatter = {
+            x: xValues,
+            y: yValues,
+            mode: 'markers',
+            name: `${data.paramNames[0]} / ${data.paramNames[1]}`,
+            // text: ['United States', 'Canada'],
+            marker: {
+                color: 'rgb(164, 194, 244)',
+                size: 10,
+                line: {
+                    color: 'black',
+                    width: 0.5
+                }
+            },
+            type: 'scatter'
+        };
+
+        return [scatterData, line];
     }
 
     private _setScatterLayout(data): any {
@@ -99,6 +147,10 @@ export class ScatterComponent implements OnInit, OnChanges {
                 // autosize: true,
                 title: '',
                 showline: false
+            },
+            showLegend: true,
+            legend: {
+                "orientation": "h"
             }
         };
 
